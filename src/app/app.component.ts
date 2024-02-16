b90c412ab88d7905914cff50f8af3f891fe4bac3
Daniel Marin <Daniel Marin>
2024-02-16 22:16:09 +0000

    feat: display silly token

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,24 +10,32 @@ import { computedAsync } from 'ngxtension/computed-async';
   standalone: true,
   imports: [RouterModule, HdWalletMultiButtonComponent],
   selector: 'cg-root',
-  template: ` <header class="px-16 pt-24 pb-8">
+  template: `
+    <header class="py-8 relative">
       <h1 class="text-center text-5xl mb-4">CG Bank</h1>
 
       <div class="flex justify-center">
         <hd-wallet-multi-button></hd-wallet-multi-button>
       </div>
-    </header>
 
-    <main>
-      {{ account()?.balance }}
-    </main>`,
+      @if (account()) {
+        <div
+          class="absolute top-4 left-4 flex justify-center items-center gap-2"
+        >
+          <img [src]="account()?.info?.image" class="w-8 h-8" />
+          <p class="text-xl">{{ account()?.balance }}</p>
+        </div>
+      }
+    </header>
+  `,
 })
 export class AppComponent {
   private readonly _shyftApiService = inject(ShyftApiService);
   private readonly _walletStore = inject(WalletStore);
   private readonly _publicKey = toSignal(this._walletStore.publicKey$);
 
-  readonly account = computedAsync(() =>
-    this._shyftApiService.getAccount(this._publicKey()?.toBase58()),
+  readonly account = computedAsync(
+    () => this._shyftApiService.getAccount(this._publicKey()?.toBase58()),
+    { requireSync: true },
   );
 }
