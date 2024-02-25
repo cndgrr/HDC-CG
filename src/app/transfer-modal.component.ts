454474ee050bb630c35d3614228da7465a4ba032
CG <cndgrrmndz@gmail.com>
2024-02-25 19:55:36 +0000

    fix: :bug:

--- src/app/transfer-modal.component.ts
+++ src/app/transfer-modal.component.ts
@@ -3,6 +3,8 @@ import {
   JustspendFormComponent,
   transferFormPayload,
 } from './transfer-form.component';
+import { injectTransactionSender } from '@heavy-duty/wallet-adapter';
+import { createTransferInstructions } from '@heavy-duty/spl-utils';
 @Component({
   selector: 'cg-bank-transfer-modal',
   template: ` <div class="px-8 py-16 pb8">
@@ -14,7 +16,25 @@ import {
   imports: [JustspendFormComponent],
 })
 export class TransferModalComponent {
+  private readonly _transactionSender = injectTransactionSender();
   onTransfer(payload: transferFormPayload) {
     console.log('Lets spend...', payload);
+
+    this._transactionSender
+      .send(({ publicKey }) =>
+        createTransferInstructions({
+          amount: payload.amount,
+          mintAddress: '7EYnhQoR9YM3N7UoaKRoA44Uy8JeaZV3qyouov87awMs',
+          receiverAddress: payload.recieverAddress,
+          senderAddress: publicKey.toBase58(),
+          fundReceiver: true,
+          memo: payload.memo,
+        }),
+      )
+      .subscribe({
+        next: (signature) => console.log(`Transaction sent: ${signature}`),
+        error: (error) => console.error(error),
+        complete: () => console.log('You just spent! 🎉'),
+      });
   }
 }
